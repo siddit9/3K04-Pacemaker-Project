2a680f8091da4f69b90bf8f6c36b02225d10ffdf
siddit9 <siddit9@mcmaster.ca>
2024-10-23 01:41:45 -0400

    EGM Data

--- main.py
+++ main.py
@@ -86,9 +86,28 @@ class mainWindow(object):
                         return
             t = messagebox.Message(self.master, message="Wrong Username/Password", type=messagebox.OK)
             t.show()
-
+class ElectrogramData:
+    def __int__(self, AS, AP, AT, TN, VS, VP, PVC, Hy, Sr,
+                UpSmoothing, DownSmoothing, ATRDur, ATRFB,
+                ATREnd, PVP):
+        self.AS = AS
+        self.AP = AP
+        self.AT = AT
+        self.TN = TN
+        self.VS = VS
+        self.VP = VP
+        self. PVC = PVC
+        self.Hy = Hy
+        self.Sr = Sr
+        self.UpSmoothing = UpSmoothing
+        self.DownSmoothing = DownSmoothing
+        self.ATRDur = ATRDur
+        self.ATRFB = ATRFB
+        self.ATREnd = ATREnd
+        self.PVP = PVP
 
 
 root = Tk()
 m = mainWindow(root)
 root.mainloop()
+
